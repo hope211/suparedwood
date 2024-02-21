0e27f963cf901fd5dd8b359926a19f02e467d60f
Patel <fahimpatel21@hotmail.com>
2024-02-22 01:02:20 +0200

    for clone

---
+++ web/src/App.jsx
@@ -0,0 +1,27 @@
+import { FatalErrorBoundary, RedwoodProvider } from '@redwoodjs/web'
+import { RedwoodApolloProvider } from '@redwoodjs/web/apollo'
+
+import FatalErrorPage from 'src/pages/FatalErrorPage'
+import Routes from 'src/Routes'
+
+import { AuthProvider, useAuth } from './auth'
+import { createClient } from '@supabase/supabase-js'
+
+const supabase = createClient(process.env.SUPABASE_URL, process.env.SUPABASE_KEY)
+
+
+import './index.css'
+
+const App = () => (
+  <FatalErrorBoundary page={FatalErrorPage}>
+    <RedwoodProvider titleTemplate="%PageTitle | %AppTitle">
+      <AuthProvider client={supabase} type="supabase">
+        <RedwoodApolloProvider useAuth={useAuth}>
+          <Routes />
+        </RedwoodApolloProvider>
+      </AuthProvider>
+    </RedwoodProvider>
+  </FatalErrorBoundary>
+)
+
+export default App
---
+++ web/src/pages/HomePage/HomePage.jsx
@@ -0,0 +1,29 @@
+import { useAuth } from '@redwoodjs/auth'
+import { Link, navigate, routes } from '@redwoodjs/router'
+import { Metadata } from '@redwoodjs/web'
+import { MetaTags } from '@redwoodjs/web'
+
+import Account from 'src/components/Account'
+import Auth from 'src/components/Auth'
+
+const HomePage = () => {
+  const { isAuthenticated } = useAuth()
+  
+  return (
+    <>
+      <Metadata title="Home" description="Home page" />
+      {!isAuthenticated ? <Auth /> : <Account />}
+
+      <h1>HomePage</h1>
+      <p>
+        Find me in <code>./web/src/pages/HomePage/HomePage.jsx</code>
+      </p>
+      <p>
+        My default route is named <code>home</code>, link to me with `
+        <Link to={routes.home()}>Home</Link>`
+      </p>
+    </>
+  )
+}
+
+export default HomePage
